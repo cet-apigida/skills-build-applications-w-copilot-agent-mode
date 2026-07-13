d0392db0faf7b9968ec678ec582d86c1b5dfe479
Alex Pi <apigida@cetaris.com>
2026-07-13 21:13:59 +0000

    feat: refactor backend structure by moving server logic to separate file

--- octofit-tracker/backend/dist/index.js
+++ octofit-tracker/backend/dist/index.js
@@ -1,28 +1,3 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-require("./config/database");
-const apiBaseUrl_1 = require("./config/apiBaseUrl");
-const users_1 = __importDefault(require("./routes/users"));
-const teams_1 = __importDefault(require("./routes/teams"));
-const activities_1 = __importDefault(require("./routes/activities"));
-const leaderboard_1 = __importDefault(require("./routes/leaderboard"));
-const workouts_1 = __importDefault(require("./routes/workouts"));
-const app = (0, express_1.default)();
-const port = Number(process.env.PORT) || 8000;
-const baseUrl = (0, apiBaseUrl_1.getApiBaseUrl)();
-app.use(express_1.default.json());
-app.use('/api/users', users_1.default);
-app.use('/api/teams', teams_1.default);
-app.use('/api/activities', activities_1.default);
-app.use('/api/leaderboard', leaderboard_1.default);
-app.use('/api/workouts', workouts_1.default);
-app.get('/api/health', (_req, res) => {
-    res.json({ status: 'ok', service: 'octofit-backend', baseUrl });
-});
-app.listen(port, () => {
-    console.log(`OctoFit backend listening on ${baseUrl}`);
-});
+require("./server");
---
+++ octofit-tracker/backend/dist/server.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+require("./config/database");
+const users_1 = __importDefault(require("./routes/users"));
+const teams_1 = __importDefault(require("./routes/teams"));
+const activities_1 = __importDefault(require("./routes/activities"));
+const leaderboard_1 = __importDefault(require("./routes/leaderboard"));
+const workouts_1 = __importDefault(require("./routes/workouts"));
+const app = (0, express_1.default)();
+const port = Number(process.env.PORT) || 8000;
+const codespaceName = process.env.CODESPACE_NAME;
+const baseUrl = codespaceName
+    ? `https://${codespaceName}-8000.app.github.dev`
+    : 'http://localhost:8000';
+app.use(express_1.default.json());
+app.use('/api/users', users_1.default);
+app.use('/api/teams', teams_1.default);
+app.use('/api/activities', activities_1.default);
+app.use('/api/leaderboard', leaderboard_1.default);
+app.use('/api/workouts', workouts_1.default);
+app.get('/api/health', (_req, res) => {
+    res.json({ status: 'ok', service: 'octofit-backend', baseUrl });
+});
+app.listen(port, () => {
+    console.log(`OctoFit backend listening on ${baseUrl}`);
+});
